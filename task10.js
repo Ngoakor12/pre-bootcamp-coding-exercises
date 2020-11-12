1d2af83a2becfe8d30c790fc6c32e1a9093c7ef6
ngoakor12 <ngoakor12@gmail.com>
2020-11-12 23:56:39 +0200

    Task 10 working

--- task10.js
+++ task10.js
@@ -1,8 +1,30 @@
-function printVowels(str){
-    var result=[];
-    while(str.indexOf()!==-1){
-        if(str.search("a")){
-            result=result.pop("a");
+function printVowels(str) {
+    var result = [];
+        if (str.toLowerCase().match("a")!==null) {
+            result.push("a");
         }
-    }
+        if (str.toLowerCase().match("e")!==null) {
+            result.push("e");
+        }
+        if (str.toLowerCase().match("i")!==null) {
+            result.push("i");
+        }
+        if (str.toLowerCase().match("o")!==null) {
+            result.push("o");
+        }
+        if (str.toLowerCase().match("u")!==null) {
+            result.push("u");
+        }
+        //print found vowels
+        for (var i = 0; i < result.length; i++) {
+            if (result.indexOf() !== result.length-1) {
+                console.log(result[i] + ", ");
+            }
+            else {
+                console.log(result[i]);
+            }
+        }
+    
 }
+//function testing
+console.log(printVowels("Ngoako"));
