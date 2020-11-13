6ebdf820526cb4cc935694e60666d8d7381a43ba
ngoakor12 <ngoakor12@gmail.com>
2020-11-13 10:33:16 +0200

    Completed task10.js

--- task10.js
+++ task10.js
@@ -1,30 +1,26 @@
 function printVowels(str) {
-    var result = [];
-        if (str.toLowerCase().match("a")!==null) {
-            result.push("a");
-        }
-        if (str.toLowerCase().match("e")!==null) {
-            result.push("e");
-        }
-        if (str.toLowerCase().match("i")!==null) {
-            result.push("i");
-        }
-        if (str.toLowerCase().match("o")!==null) {
-            result.push("o");
-        }
-        if (str.toLowerCase().match("u")!==null) {
-            result.push("u");
-        }
-        //print found vowels
-        for (var i = 0; i < result.length; i++) {
-            if (result.indexOf() !== result.length-1) {
-                console.log(result[i] + ", ");
-            }
-            else {
-                console.log(result[i]);
-            }
-        }
-    
+    var result = 0;
+    console.log("Vowels in " + str + ":")
+    if (str.toLowerCase().match("a") !== null) {
+        result = "a";
+        console.log(result);
+    }
+    if (str.toLowerCase().match("e") !== null) {
+        result = "e";
+        console.log(result);
+    }
+    if (str.toLowerCase().match("i") !== null) {
+        result = "i";
+        console.log(result);
+    }
+    if (str.toLowerCase().match("o") !== null) {
+        result = "o";
+        console.log(result);
+    }
+    if (str.toLowerCase().match("u") !== null) {
+        result = "u";
+        console.log(result);
+    }
 }
 //function testing
-console.log(printVowels("Ngoako"));
+printVowels("NgOakORamOkgOpa");
