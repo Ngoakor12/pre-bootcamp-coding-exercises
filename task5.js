708522331cc3d24eb0530709417d39e823b0303c
ngoakor12 <ngoakor12@gmail.com>
2020-11-14 21:41:56 +0200

    fixed variable naming

--- task5.js
+++ task5.js
@@ -1,8 +1,7 @@
 function areaOfTriangle(x, y, z) {
-    var area,
-        SP;       //SemiPerimeter  
-    SP = (1 / 2) * (x + y + z);
-    area = Math.sqrt(SP * (SP - x) * (SP - y) * (SP - z));
+    var area, semiPerimeter;
+    semiPerimeter = (1 / 2) * (x + y + z);
+    area = Math.sqrt(semiPerimeter * (semiPerimeter - x) * (semiPerimeter - y) * (semiPerimeter - z));
     return area;
 }
 //function testing
