91d16cf5a78d17424f33107ce385cd0078af42b7
ngoakor12 <ngoakor12@gmail.com>
2020-11-16 14:19:04 +0200

    removed useless comments, changed variable names

--- task11.js
+++ task11.js
@@ -1,30 +1,30 @@
 function commonCharacters(string1, string2) {
-    var result = []; //array to store common characters
-    //dealing with case sensetivity by converting strings to lowercase
+    var common = [];
+    //dealing with case sensitivity by converting strings to lowercase
     string1 = string1.toLowerCase();
     string2 = string2.toLowerCase();
     //assigning common characters to an array
     for (var i = 0; i < string1.length; i++) {
         for (var k = 0; k < string2.length; k++) {
             if (string1[i] === string2[k]) {
-                result.push(string1[i]);
+                common.push(string1[i]);
             }
         }
     }
     //checking for duplicates
-    var temp = []; //temporary array to store the checked array
-    for (var a = 0; a < result.length; a++) {
-        if (result[a] !== result[a + 1]) {
-            temp.push(result[a]);
+    var temporary = [];
+    for (var a = 0; a < common.length; a++) {
+        if (common[a] !== common[a + 1]) {
+            temporary.push(common[a]);
         }
     }
+
     //printing the common characters
-    //overwriting the duplicate array with the unduplicated one
-    result = temp;
+    common = temporary;
     //converting array to a string separating elements by a comma
-    result = result.join(", ");
+    common = common.join(", ");
     console.log("Inputs: " + string1 + ", " + string2);
-    console.log("Common characters: " + result);
+    console.log("Common characters: " + common);
 }
 //function testing
 commonCharacters("house", "computers");
