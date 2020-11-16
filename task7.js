bdc4b91addc214d51461528448f342d37594b296
ngoakor12 <ngoakor12@gmail.com>
2020-11-16 08:36:59 +0200

    changed function names

--- task7.js
+++ task7.js
@@ -1,13 +1,13 @@
 //function for converting fareignheit to celsius
-function toCelsius(temp) {
+function fareignheitToCelsius(temp) {
     temp = (temp - 32) * 5 / 9;
     return Math.round(temp);
 }
 //function for converting celsius to fareignheit
-function toFareignheit(temp) {
+function celsiusToFareignheit(temp) {
     temp = temp * 9 / 5 + 32;
     return Math.round(temp);
 }
 //function testing
-console.log(toCelsius(75));
-console.log(toFareignheit(27));
+console.log(fareignheitToCelsius(75));
+console.log(celsiusToFareignheit(27));
