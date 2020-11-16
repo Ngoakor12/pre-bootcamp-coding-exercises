81d2ae768bf6b9b2fd45d8726aadb526ab228a34
ngoakor12 <ngoakor12@gmail.com>
2020-11-16 14:14:53 +0200

    removed useless comments, changed variable names

--- task7.js
+++ task7.js
@@ -1,12 +1,11 @@
-//function for converting fareignheit to celsius
-function fareignheitToCelsius(temp) {
-    temp = (temp - 32) * 5 / 9;
-    return Math.round(temp);
+function fareignheitToCelsius(temperature) {
+    temperature = (temperature - 32) * 5 / 9;
+    return Math.round(temperature);
 }
-//function for converting celsius to fareignheit
-function celsiusToFareignheit(temp) {
-    temp = temp * 9 / 5 + 32;
-    return Math.round(temp);
+
+function celsiusToFareignheit(temperature) {
+    temperature = temperature * 9 / 5 + 32;
+    return Math.round(temperature);
 }
 //function testing
 console.log(fareignheitToCelsius(75));
