b29bcad3bd1b1590d0a68e0675bd4c2f12664f09
ngoakor12 <ngoakor12@gmail.com>
2021-03-17 09:13:15 +0200

    cleaned code

--- task8.js
+++ task8.js
@@ -1,16 +1,25 @@
 function convertToTime(x) {
-    var hours = 0, minutes = 0;
-    if (x >= 0) {        //to filter negative numbers
-        while (x >= 60) {
-            hours += 1;
-            x -= 60;
-        }
-        minutes = x;
-        return hours + " hours, " + minutes + " minutes";
-    }
-    else {
-        return "Error: No negative numbers allowed."
-    }
+	var hours = 0,
+		minutes = 0;
+	if (x > 0) {
+		//to filter negative numbers
+		while (x >= 60) {
+			hours += 1;
+			x -= 60;
+		}
+		minutes = x;
+		if (hours == 1 && minutes == 1) {
+			return hours + " hour, " + minutes + " minute";
+		} else if (hours == 1 && minutes != 1) {
+			return hours + " hour, " + minutes + " minutes";
+		} else if (hours != 1 && minutes == 1) {
+			return hours + " hours, " + minutes + " minute";
+		} else {
+			return hours + " hours, " + minutes + " minutes";
+		}
+	} else {
+		return "Error: No negative numbers allowed.";
+	}
 }
 //function testing
-console.log(convertToTime(72));
+console.log(convertToTime(121));
