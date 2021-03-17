95a002e28cd03579a6b863c4c9f57b410fd57749
ngoakor12 <ngoakor12@gmail.com>
2021-03-17 09:23:13 +0200

    improved function

--- task6.js
+++ task6.js
@@ -1,28 +1,12 @@
-//function taking 3 numbers and returning the biggest
-function biggestOfThreeNumbers(x, y, z) {
-	var biggest;
-	if (x > y && x > z) {
-		biggest = x;
-	} else if (y > x && y > z) {
-		biggest = y;
-	} else {
-		biggest = z;
-	}
-	return biggest;
-}
-//function testing
-console.log(biggestOfThreeNumbers(70, 30, 47));
-
-// BONUS: function taking many numbers and returning the biggest
-function biggestOfManyNumbers(arr = []) {
-	var i = 0;
-	var biggest = 0;
-	for (i = 0; i < arr.length; i++) {
-		if (biggest <= arr[i]) {
-			biggest = arr[i];
+function biggestNumber(...numbers) {
+	let biggestNum = numbers[0];
+	for (let i = 0; i < numbers.length; i++) {
+		if (biggestNum < numbers[i]) {
+			biggestNum = numbers[i];
 		}
 	}
-	return biggest;
+	console.log(biggestNum);
+	return biggestNum;
 }
-//function testing
-console.log(biggestOfManyNumbers([100, , 1000, 5, 0, 9800, 560]));
+
+biggestNumber(10000, 56, 4, 787, 322);
