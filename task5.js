c40af9c36f08da8a7d5ad99483e7f8e280609759
ngoakor12 <ngoakor12@gmail.com>
2021-03-17 09:20:40 +0200

    improved variable names

--- task5.js
+++ task5.js
@@ -1,7 +1,12 @@
-function areaOfTriangle(x, y, z) {
-	var area, SP; //SemiPerimeter
-	SP = (1 / 2) * (x + y + z);
-	area = Math.sqrt(SP * (SP - x) * (SP - y) * (SP - z));
+function areaOfTriangle(side1, side2, side3) {
+	var area, semiPerimeter;
+	semiPerimeter = (1 / 2) * (side1 + side2 + side3);
+	area = Math.sqrt(
+		semiPerimeter *
+			(semiPerimeter - side1) *
+			(semiPerimeter - side2) *
+			(semiPerimeter - side3)
+	);
 	return area;
 }
 //function testing
